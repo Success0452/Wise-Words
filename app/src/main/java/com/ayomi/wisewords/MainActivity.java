9398142becfdcd335a407319012bf1a4d8ce383b
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-07-15 16:17:59 +0100

    Initial commit

---
+++ app/src/main/java/com/ayomi/wisewords/MainActivity.java
@@ -0,0 +1,122 @@
+package com.ayomi.wisewords;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+
+        Menu menu = mNavigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+        menu.findItem(R.id.nav_profile).setVisible(false);
+
+        mNavigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        mNavigationView.setNavigationItemSelectedListener(this);
+        mNavigationView.setCheckedItem(R.id.nav_home);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_cycle:
+                Toast.makeText(this, "clicked Cycle", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_bus:
+                Toast.makeText(this, "Bus", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_car:
+                Toast.makeText(this, "Clicked Car", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_login:
+                Toast.makeText(this, "Clicked Login", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Clicked Logout", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_profile:
+                Toast.makeText(this, "Clicked Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_share:
+                Toast.makeText(this, "Clicked Share", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_rate:
+                Toast.makeText(this, "Clicked Rate", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+
+    public void wisdomQuotes(View view) {
+        Toast.makeText(this, "Wisdom Quote clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void otherApps(View view) {
+        Toast.makeText(this, "Other App clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void about(View view) {
+        Toast.makeText(this, "About clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void stories(View view) {
+        Toast.makeText(this, "Stories clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void writeUps(View view) {
+        Toast.makeText(this, "WriteUps clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void motivationalQuotes(View view) {
+        Toast.makeText(this, "Motivational Quote clicked", Toast.LENGTH_SHORT).show();
+    }
+}
