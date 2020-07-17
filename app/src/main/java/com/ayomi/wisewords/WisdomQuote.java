edffb3941a8b6c94776f016c6c3ef87b45661cd1
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-07-17 02:23:34 +0100

    udate

--- app/src/main/java/com/ayomi/wisewords/WisdomQuote.java
+++ app/src/main/java/com/ayomi/wisewords/WisdomQuote.java
@@ -10,5 +10,6 @@ public class WisdomQuote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wisdom_quote);
+
     }
 }
