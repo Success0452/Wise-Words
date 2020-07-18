bf1c821226fa0f8d0b1bc734593634c234bda3d8
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-07-18 14:25:23 +0100

    update

--- app/src/main/java/com/ayomi/wisewords/Stories.java
+++ app/src/main/java/com/ayomi/wisewords/Stories.java
@@ -1,74 +1,90 @@
 package com.ayomi.wisewords;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class Stories extends AppCompatActivity {
 
-    EditText author, quotes;
-    Button btnsubmit;
+    private RecyclerView recyclerView;
+    private ArrayList<SetQuotes> arrayList;
+    private FirebaseRecyclerOptions<SetQuotes> options;
+    private FirebaseRecyclerAdapter<SetQuotes,FirebaseViewHolder> adapter;
+    private DatabaseReference databaseReference;
 
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
 
-        author = findViewById(R.id.edtname);
-        quotes = findViewById(R.id.edt_quotes);
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        arrayList = new ArrayList<SetQuotes>();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Stories");
+        //offline
+        databaseReference.keepSynced(true);
 
-        btnsubmit = findViewById(R.id.btnsubmit);
+        options = new FirebaseRecyclerOptions.Builder<SetQuotes>().setQuery(databaseReference,SetQuotes.class).build();
 
-        database = FirebaseDatabase.getInstance();
-
-
-        //myRef.setValue("Hello, World!");
-
-        btnsubmit.setOnClickListener(new View.OnClickListener() {
+        adapter = new FirebaseRecyclerAdapter<SetQuotes, FirebaseViewHolder>(options) {
             @Override
-            public void onClick(View v) {
+            protected void onBindViewHolder(@NonNull FirebaseViewHolder firebaseViewHolder, int i, @NonNull final SetQuotes setQuotes) {
 
-                if(!(author.getText().toString().equals("") || quotes.getText().toString().equals(""))){
-                    getValues();
-                    //setContentView(R.layout.activity_main2);
-                } else{
-                    Toast.makeText(Stories.this, "All field required", Toast.LENGTH_SHORT).show();
-                }
+                firebaseViewHolder.author.setText(setQuotes.getName());
+                firebaseViewHolder.quotes.setText(setQuotes.getQuotes());
+
+                firebaseViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Stories.this, Motivational.class);
+                        intent.putExtra("name", setQuotes.getName());
+                        intent.putExtra("quotes",setQuotes.getQuotes());
+
+                        startActivity(intent);
+                    }
+                });
 
             }
-        });
 
-    }
+            @NonNull
+            @Override
+            public FirebaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new FirebaseViewHolder(LayoutInflater.from(Stories.this).inflate(R.layout.row,parent,false));
+            }
+        };
 
-    private void getValues(){
 
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Stories");
 
-        String uniqueId = myRef.push().getKey();
-
-        Quotes user = new Quotes();
-        user.setName(author.getText().toString());
-        user.setQuotes(quotes.getText().toString());
-
-        myRef.child(uniqueId).setValue(user);
-        Toast.makeText(Stories.this, "Data Inputed...",Toast.LENGTH_SHORT).show();
-
-    }
-
-    public void btnView(View view) {
-    }
-
-    public void btnSubmit(View view) {
+        recyclerView.setAdapter(adapter);
     }
 }
